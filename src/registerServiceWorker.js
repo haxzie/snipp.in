6d7507ab1fb3e911b20c7304dfbb1c5e254a444a
haxzie <musthu.gm@gmail.com>
2021-05-26 11:54:04 +0530

    Add triggers

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,6 +2,8 @@
 
 import { register } from 'register-service-worker'
 
+const VERSION = process.env.COMMIT_REF;
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
