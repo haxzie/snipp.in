905aa2c548bafe6d991b2325141d3db8f393905e
urangurang <ddwax89@gmail.com>
2021-06-07 00:10:46 +0900

    Add api communication for fetching stock information

--- src/main.js
+++ src/main.js
@@ -10,12 +10,16 @@ import VTooltip from "v-tooltip";
 import router from "./routes";
 import './registerServiceWorker'
 import Notifications from 'vue-notification'
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 Vue.component("simplebar", simplebar);
 Vue.use(vClickOutside);
 Vue.use(VTooltip);
 Vue.use(require('vue-shortkey'));
 Vue.use(Notifications)
+Vue.use(VueAxios, axios)
+
 
 WebFont.load({ google: { families: ["Inter", "Fira Code"] } });
 new Vue({
